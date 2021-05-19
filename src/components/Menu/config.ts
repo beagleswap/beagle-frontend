0589e87ec9a68df3a8b0044630b8049356f80047
beagle-snoopy <snoopy@beagleswap.com>
2021-05-19 13:09:10 +0800

    beagle init

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -38,48 +38,15 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: '/pools',
   },
-  // {
-  //   label: 'Prediction',
-  //   icon: 'PredictionsIcon',
-  //   href: '/prediction',
-  //   status: {
-  //     text: 'BETA',
-  //     color: 'warning',
-  //   },
-  // },
   {
     label: 'Lottery',
     icon: 'TicketIcon',
     href: '/lottery',
+    status: {
+      text: 'BETA',
+      color: 'warning',
+    }
   },
-  // {
-  //   label: 'Collectibles',
-  //   icon: 'NftIcon',
-  //   href: '/collectibles',
-  // },
-  // {
-  //   label: 'Team Battle',
-  //   icon: 'TeamBattleIcon',
-  //   href: '/competition',
-  // },
-  // {
-  //   label: 'Teams & Profile',
-  //   icon: 'GroupsIcon',
-  //   items: [
-  //     {
-  //       label: 'Leaderboard',
-  //       href: '/teams',
-  //     },
-  //     {
-  //       label: 'Task Center',
-  //       href: '/profile/tasks',
-  //     },
-  //     {
-  //       label: 'Your Profile',
-  //       href: '/profile',
-  //     },
-  //   ],
-  // },
   {
     label: 'Info',
     icon: 'InfoIcon',
@@ -101,11 +68,6 @@ const config: MenuEntry[] = [
         href: 'https://pancakeswap.info/accounts',
       },
     ],
-  },
-  {
-    label: 'IFO',
-    icon: 'IfoIcon',
-    href: '/ifo',
   },
   {
     label: 'More',
